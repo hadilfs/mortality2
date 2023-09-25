0bd4889f44a61de87a045cf9d83742110c40fdd3
hadilfs <145138634+hadilfs@users.noreply.github.com>
2023-09-25 15:48:02 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ if selectedchart == "Bar Chart":
     st.subheader("Using a Bar Chart")
     show_barchart=st.button("Click to Show Bar Chart")
     if show_barchart:
-        df = pd.read_csv(r"C:\Users\User\Documents\streamlit\lebanon_mortality_2.csv")
+        df = pd.read_csv("https://raw.githubusercontent.com/hadilfs/mortality2/main/lebanon_mortality_2.csv")
         fig = px.bar(df,x='Year',y='Deaths',color="Year")
         st.plotly_chart(fig) 
         st.caption("***This bar chart shows the mortality rate in lebanon from 1960 to 2021***")
@@ -17,7 +17,7 @@ elif selectedchart == "Scatter Plot":
         st.subheader("Using a Scatter plot")
         show_scatterplot=st.button("Click to Show Scatter Plot")
         if show_scatterplot:
-            df = pd.read_csv(r"C:\Users\User\Documents\streamlit\lebanon_mortality_2.csv")
+            df = pd.read_csv("https://raw.githubusercontent.com/hadilfs/mortality2/main/lebanon_mortality_2.csvv")
             fig1 = px.scatter(df, x="Year", y="Deaths",color="Deaths",color_continuous_scale="Reds")
             st.plotly_chart(fig1)
             st.caption("***This scatter plot shows the mortality rate in lebanon from 1960 to 2021***")
