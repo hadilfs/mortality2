0cf523a19fc1c8c87d78ee22fd13b34fcbca0a95
hadilfs <145138634+hadilfs@users.noreply.github.com>
2023-10-11 00:58:00 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ if selectedchart == "Bar Chart":
     fig = px.bar(filtered_df,x='Year',y='Deaths')
     highest_value = st.checkbox("Highlight Highest Value")
     st.caption("***This bar chart shows the mortality rate in lebanon from 1960 to 2021. Each bar refers to a year and its height represents the rate of death in that year. Picking a certain time period using the range slider above, allows for a thorough analysis of the data.***")
-    st.caption("***Checking on the 'Highlight Highest Value' box allows you to observe the bar which refers to the highest death rate in the year range you chose. Notably, the bar chart shows that in the year 1982 there was a sudden increase in the death rate which could be explained by the fact that in this year, the Israel Defense forces invaded the country in the midst of the Civil War. Also, a gradual increase is observed between the years 2019 and 2021 due to the COVID-19 pandemic.***")
+    st.caption("***Checking on the 'Highlight Highest Value' box allows you to observe the bar which refers to the highest death rate in the year range you chose. Notably, the bar chart shows that in the year 1976 there was a sudden increase in the death rate which could be explained by the fact that in this year, the Lebanese Civil War was at its climax. Additionally, in 1982 the Israel Defense forces invaded the country in the midst of the War, which explain the second increase in that year. Also, a gradual increase is observed between the years 2019 and 2021 due to the COVID-19 pandemic.***")
     if highest_value:
           max_death = filtered_df[filtered_df['Deaths'] == filtered_df['Deaths'].max()]
           max_year = max_death['Year'].values
@@ -22,7 +22,7 @@ elif selectedchart == "Scatter Plot":
         fig = px.scatter(filtered_df, x="Year", y="Deaths")
         highest_value = st.checkbox("Highlight Highest Value")
         st.caption("***This scatter plot shows the mortality rate in lebanon from 1960 to 2021. The X-coordinate of each point refers to a year and the Y-coordinate represents the rate of death in that year. Picking a certain time period using the range slider above, allows for a thorough analysis of the data.***")
-        st.caption("***Checking on the 'Highlight Highest Value' box allows you to observe the point which refers to the highest death rate in the year range you chose. Notably, the scatter plot that in the year 1982 there was a sudden increase in the death rate which could be explained by the fact that in this year, the Israel Defense forces invaded the country in the midst of the Civil War. Also, a gradual increase is observed between the years 2019 and 2021 due to the COVID-19 pandemic.***")
+        st.caption("***Checking on the 'Highlight Highest Value' box allows you to observe the point which refers to the highest death rate in the year range you chose. Notably, the scatterplot shows that in the year 1976 there was a sudden increase in the death rate which could be explained by the fact that in this year, the Lebanese Civil War was at its climax. Additionally, in 1982 the Israel Defense forces invaded the country in the midst of the War, which explain the second increase in that year. Also, a gradual increase is observed between the years 2019 and 2021 due to the COVID-19 pandemic.***")
         if highest_value:
           max_death = filtered_df[filtered_df['Deaths'] == filtered_df['Deaths'].max()]
           max_year = max_death['Year'].values
